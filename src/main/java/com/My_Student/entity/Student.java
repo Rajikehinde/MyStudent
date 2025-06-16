470d072523b116aeb0f99f9d4922d3e45ddd7960
Kehinde Raji <Kehinderaji28@gmail.com>
2025-06-16 07:32:41 -0700

    creation of entity and repository

---
+++ src/main/java/com/My_Student/entity/Student.java
@@ -0,0 +1,21 @@
+package com.My_Student.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String firstName;
+    private String surName;
+    private String middleName;
+}
