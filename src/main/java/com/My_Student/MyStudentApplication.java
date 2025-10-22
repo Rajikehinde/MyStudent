3db79f3fed3ad9c009f733cb01f8e28af61f909b
Kehinde Raji <Kehinderaji28@gmail.com>
2025-10-22 01:36:21 -0700

    OpenApiDefinition in mainClass for proper docs description and @Tag,@Operation and @ApiResponses in controller for proper docs descriptions

--- src/main/java/com/My_Student/MyStudentApplication.java
+++ src/main/java/com/My_Student/MyStudentApplication.java
@@ -1,11 +1,36 @@
 package com.My_Student;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "My Student microservice REST API Documentation",
+                description = "My Student) microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact  (
+                        name = "Kehinde Raji",
+                        email = "kerahnTech@gmail.com",
+                        url = "https://www.mystudent.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.mystudent.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "My Student microservice REST API Documentation",
+                url = "https://www.mystudent.com"
+        )
+)
 public class MyStudentApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/My_Student/controller/StudentController.java
+++ src/main/java/com/My_Student/controller/StudentController.java
@@ -5,6 +5,9 @@ import com.My_Student.dto.StudentRequestDto;
 import com.My_Student.dto.Response;
 import com.My_Student.service.ServiceImplementation;
 import com.My_Student.utitlities.Utils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -16,7 +19,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@Tag(
+        name = "CRUD REST APIs for Student Management",
+        description = "CRUD REST APIs to CREATE, UPDATE, FETCH AND DELETE student details"
+)
 @RestController
 @RequestMapping(path = "api/student/", produces = {MediaType.APPLICATION_JSON_VALUE})
 @Validated
@@ -28,32 +34,81 @@ public class StudentController {
         this.serviceImplementation = serviceImplementation;
     }
 
+    @Operation(
+            summary = "Student Registration REST API",
+            description = "REST API to register new Student"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status CREATED"
+    )
     @PostMapping("/Registration")
     public ResponseEntity<Response> registerStudent(@Valid @RequestBody StudentRequestDto candidateInformationStudentRequestDto){
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new Response(Utils.STATUS_201, Utils.MESSAGE_201, DataView.builder().build()));
     }
 
+    @Operation(
+            summary = "List All Students REST API",
+            description = "REST API to list all registered Students"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+    )
     @GetMapping("/all/students{field}")
     Page<Response> listAllStudents(@PathVariable String field){
         Page<Response> allStudents = serviceImplementation.listStudents(field);
         return allStudents;
     }
+
+    @Operation(
+            summary = "List All Students REST API",
+            description = "REST API to list all registered Students"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+    )
     @GetMapping("/all/stu")
     public List<Response> listAllStudents(){
         return serviceImplementation.listAllStudents();
     }
 
+    @Operation(
+            summary = "Search Student REST API",
+            description = "REST API to search a registered Student by ID"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+    )
     @GetMapping("/student/{userFetching}")
     public Response searchForStudent(@PathVariable("userFetching") Long id){
         return serviceImplementation.searchForStudent(id);
     }
 
+    @Operation(
+            summary = "Update Student Information REST API",
+            description = "REST API to update existing Student information"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+    )
     @PostMapping("update")
     public Response updateStudents(@Valid @RequestBody StudentRequestDto studentStudentRequestDtoInformation){
         return serviceImplementation.updateStudentInformation(studentStudentRequestDtoInformation);
     }
 
+    @Operation(
+            summary = "Delete Student REST API",
+            description = "REST API to delete a registered Student by ID"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status OK"
+    )
     @DeleteMapping("delete")
     public Response deleteStudent (Long id){
         return serviceImplementation.deleteStudent(id);
