d6e3282d88d4ccb4c5856b09020c61d50b953237
Kehinde Raji <Kehinderaji28@gmail.com>
2025-10-22 01:50:17 -0700

    Schema description for the dtos

--- src/main/java/com/My_Student/dto/Response.java
+++ src/main/java/com/My_Student/dto/Response.java
@@ -1,5 +1,6 @@
 package com.My_Student.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,9 +10,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Schema(
+        name = "Response",
+        description = "Schema to hold response information")
 public class Response {
+    @Schema(
+            description = "Response code in the response", example = "200"
+    )
     private String responseCode;
+
+    @Schema(
+            description = "Response message in the response", example = "Student created successfully"
+    )
     private String responseMessage;
+
     private DataView dataView;
 
 //    private String statusCode;
--- src/main/java/com/My_Student/dto/StudentRequestDto.java
+++ src/main/java/com/My_Student/dto/StudentRequestDto.java
@@ -2,6 +2,7 @@ package com.My_Student.dto;
 
 import com.My_Student.entity.Gender;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -17,29 +18,52 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Schema(
+        name = "StudentRequest",
+        description = "Schema to hold Student  information"
+)
 public class StudentRequestDto {
 
+    @Schema(
+            description = "First Name of the Student ", example = "Kehinde"
+    )
     @Size(min = 5, max = 30, message = "The length of the first name should be between 5 and 30")
     @NotNull(message = "Firstname cannot be null or empty!")
     private String firstName;
 
+    @Schema(
+            description = "Surname of the Student ", example = "Raji"
+    )
     @Size(min = 5, max = 30, message = "The length of the  Surname should be between 5 and 30")
     @NotNull(message = "Surname cannot be null or empty!")
     private String surName;
 
+    @Schema(
+            description = "Middle Name of the Student ", example = "Husaini"
+    )
     @Size(min = 5, max = 30, message = "The length of the Middle name should be between 5 and 30")
     @NotNull(message = "Middle name cannot be null or empty!")
     private String middleName;
 
+    @Schema(
+            description = "Date of Birth of the Student ", example = "1999-08-29"
+    )
     @NotNull(message = "Date of birth cannot be null or empty!")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateOfBirth;
 
+
     private int age;
 
+    @Schema(
+            description = "Gender of the Student ", example = "MALE/FEMALE/OTHER"
+    )
     @NotNull(message = "Gender is required")
     private Gender gender;
 
+    @Schema(
+            description = "Email of the Student ", example = "Kehinderaji28@gmail.com"
+    )
     @NotNull(message = "Email cannot be null or empty")
     @Email(message = "Email address should be valid")
     private String email;
